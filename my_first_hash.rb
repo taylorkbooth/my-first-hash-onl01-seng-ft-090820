425e95476fa7ad351545bec3970325c81c8bfdde
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 23:43:06 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,7 +20,6 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
@@ -30,15 +29,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
 shipping_manifest = {"muskets" => 2}
 shipping_manifest = {"gun powder" => 4}
 
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
-end
 hash
+end
