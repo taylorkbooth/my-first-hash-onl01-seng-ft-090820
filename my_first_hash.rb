620c459d46a1d24dfca35ca0fecae464b4ba711d
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 23:48:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,7 +29,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-shipping_manifest = {"muskets" => 2}
-shipping_manifest = {"gun powder" => 4}
+the_manifest = {"muskets" => 2}
+the_manifest = {"gun powder" => 4}
 
 end
