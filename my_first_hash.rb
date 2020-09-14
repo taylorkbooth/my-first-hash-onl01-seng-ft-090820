dfcca1772c8d91a45a69cc12cbca5b37f2db8890
Taylor Booth <taylorkbooth@gmail.com>
2020-09-14 23:52:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -29,8 +29,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-the_manifest["muskets"]= 2
-the_manifest["gun powder"]= 4
+shipping_manifest["muskets"]= 2
+shipping_manifest["gun powder"]= 4
 
-
+shipping_manifest
 end
